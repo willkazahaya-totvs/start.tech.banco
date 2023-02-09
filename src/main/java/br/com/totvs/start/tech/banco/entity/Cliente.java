399a973ed4bd44365935d07fee8869ccac1525d7
CTT <gustavosilva90@gmail.com>
2023-02-09 11:08:11 -0300

    Criação de Atributos Cliente.

--- src/main/java/br/com/totvs/start/tech/banco/entity/Cliente.java
+++ src/main/java/br/com/totvs/start/tech/banco/entity/Cliente.java
@@ -1,4 +1,11 @@
 package br.com.totvs.start.tech.banco.entity;
 
 public class Cliente {
-}
+
+    private String id;
+    private String nome;
+    private String cpf;
+    private String profissao;
+
+    
+};
